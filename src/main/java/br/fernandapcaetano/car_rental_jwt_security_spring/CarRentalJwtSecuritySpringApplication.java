496a5d9fb2c4fbd14250aec3422f7788742d25af
Maria Fernanda <fernandamariacaetanopereira@gmail.com>
2024-09-17 12:41:34 -0300

    Estrutura de pastas

---
+++ src/main/java/br/fernandapcaetano/car_rental_jwt_security_spring/CarRentalJwtSecuritySpringApplication.java
@@ -0,0 +1,17 @@
+package br.fernandapcaetano.car_rental_jwt_security_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class CarRentalJwtSecuritySpringApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalJwtSecuritySpringApplication.class, args);
+
+    
+	}    
+
+}
---
+++ src/test/java/br/fernandapcaetano/car_rental_jwt_security_spring/CarRentalJwtSecuritySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fernandapcaetano.car_rental_jwt_security_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalJwtSecuritySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
